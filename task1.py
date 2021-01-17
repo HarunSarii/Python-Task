ffce1a70424ef177a54436fa31d97e57dcd9d4c9
HarunSarii <harunsari14@gmail.com>
2021-01-18 00:09:52 +0300

    python task 1 and 2

---
+++ task1.py
@@ -0,0 +1,7 @@
+ #task 1
+name = "Harun"
+username = input("Enter your first name: ")
+if username == name:
+    print("Hello, Harun! The password is : W@12")
+else:
+    print("Hello,", username, "See you later.")
